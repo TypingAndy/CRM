34a555a363da386afb6dc42b5d266484f23a5d5a
TypingAndy <andreas.georg@outlook.com>
2025-04-18 22:09:10 +0200

    added popup

---
+++ CRM/src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatDialog, MatDialogActions, MatDialogContent } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog-add-user',
+  standalone: true,
+  imports: [MatDialogContent, MatDialogActions,],
+  templateUrl: './dialog-add-user.component.html',
+  styleUrl: './dialog-add-user.component.scss'
+})
+export class DialogAddUserComponent {
+
+}
