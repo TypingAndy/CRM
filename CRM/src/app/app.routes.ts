9a3c76749e541add71cdc9367f8a221f527e3ab7
TypingAndy <andreas.georg@outlook.com>
2025-04-18 20:23:16 +0200

    added add user btn, added routing, added user and dashboard path

--- CRM/src/app/app.routes.ts
+++ CRM/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'user', component: UserComponent },
+];
