50c34f0371d10b0400d371d8122c592a3e24769b
TypingAndy <andreas.georg@outlook.com>
2025-04-19 08:47:34 +0200

    build ui user input, create class for users

--- CRM/src/app/dialog-add-user/dialog-add-user.component.ts
+++ CRM/src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { MatDialog, MatDialogActions, MatDialogContent } from '@angular/material/dialog';
+import { MatDialogActions, MatDialogContent } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import {
+  MatDatepicker,
+  MatDatepickerModule,
+} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [MatDialogContent, MatDialogActions,],
+  imports: [
+    MatDialogContent,
+    MatDialogActions,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatDatepicker,
+    MatNativeDateModule,
+  ],
   templateUrl: './dialog-add-user.component.html',
-  styleUrl: './dialog-add-user.component.scss'
+  styleUrl: './dialog-add-user.component.scss',
 })
-export class DialogAddUserComponent {
-
-}
+export class DialogAddUserComponent {}
